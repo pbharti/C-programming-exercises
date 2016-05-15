82bc50d44718a6edd865e01d52efa1786b9daa04
pankaj <pankajbharti.lfc@gmail.com>
2016-05-15 13:00:02 +0530

    Commit for BST

--- bst/inc/bst.h
+++ bst/inc/bst.h
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 typedef struct node_s {
-    struct node_s *link;
+    struct node_s *left;
+    struct node_s *right;
     int data;
 }node_t;
 
@@ -11,5 +12,5 @@ void IterateInorder(node_t *root);
 void IteratePreorder(node_t *root);
 void IteratePostorder(node_t *root);
 node_t *Search(node_t *root, int data);
-int Add(node_t *root, int data);
+node_t *Add(node_t *root, int data);
 int Remove(node_t *root, int data);
--- bst/src/bst.c
+++ bst/src/bst.c
@@ -22,5 +22,51 @@ void IteratePostorder(node_t *root)
 {
      if(!root)
          return;
+     IteratePostorder(root->left);
+     IteratePostorder(root->right);
+     printf("%d ", root->data);
+}
 
+node_t * Add(node_t *root, int data)
+{
+    if(!root) {
+        node_t *temp = (node_t *)malloc(sizeof(node_t));
+        if(!temp) {
+            printf("\n memory allocation failed \n");
+            return NULL;
+        }
+        temp->data = data;
+        temp->left = NULL;
+        temp->right = NULL;
+        root = temp;
+        return root;
+    }
+     if(data <= root->data)
+         root->left = Add(root->left, data);
+     else
+         root->right = Add(root->right, data);
+}
+
+int main()
+{
+     node_t *root = NULL;
+     root = Add(root, 10);
+     root = Add(root, 15);
+     root = Add(root, 8);
+     root = Add(root, 25);
+     root = Add(root, 1);
+     root = Add(root, 5);
+     root = Add(root, 9);
+     root = Add(root, 3);
+     root = Add(root, 100);
+     printf("\n######### Inorder Traversal #########\n");
+     IterateInorder(root);
+     printf("\n######### End ######################\n");
+     printf("\n######### Preorder Traversal #########\n");
+     IteratePreorder(root);
+     printf("\n######### End ######################\n");
+     printf("\n######### Postorder Traversal #########\n");
+     IteratePostorder(root);
+     printf("\n######### End ######################\n");
+     return 0;
 }
