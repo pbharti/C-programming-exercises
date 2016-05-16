19d2e7b179f6874c7937964a8a67c30acfcd83e1
pankaj <pankajbharti.lfc@gmail.com>
2016-05-16 12:52:04 +0530

    Coomit for bst with Search API

--- bst/src/bst.c
+++ bst/src/bst.c
@@ -56,11 +56,16 @@ int Delete(node_t *root, int data)
 #endif
 }
 
-node_t *Search(node_t *root)
+node_t *Search(node_t *root, int data)
 {
     if(!root)
         return NULL;
-    else if()
+    else if (root->data == data)
+        return root;
+    else if(root->data <= data)
+        return(Search(root->right, data));
+    else
+        return(Search(root->left, data));
 }
 node_t *FindMax(node_t * root)
 {
@@ -82,36 +87,48 @@ node_t *FindMin(node_t *root)
 }
 int main()
 {
-     node_t *root = NULL;
-     root = Add(root, 10);
-     root = Add(root, 15);
-     root = Add(root, 8);
-     root = Add(root, 25);
-     root = Add(root, 1);
-     root = Add(root, 5);
-     root = Add(root, 9);
-     root = Add(root, 3);
-     root = Add(root, 100);
-     printf("\n######### Inorder Traversal #########\n");
-     IterateInorder(root);
-     printf("\n######### End ######################\n");
-     printf("\n######### Preorder Traversal #########\n");
-     IteratePreorder(root);
-     printf("\n######### End ######################\n");
-     printf("\n######### Postorder Traversal #########\n");
-     IteratePostorder(root);
-     printf("\n######### End ######################\n");
-     node_t *MinNode = NULL;
-     MinNode = FindMin(root);
-     if(!MinNode)
-         printf("\n Failed to get smallest Node \n");
-     else
+    node_t *root = NULL;
+    root = Add(root, 10);
+    root = Add(root, 15);
+    root = Add(root, 8);
+    root = Add(root, 25);
+    root = Add(root, 1);
+    root = Add(root, 5);
+    root = Add(root, 9);
+    root = Add(root, 3);
+    root = Add(root, 100);
+    printf("\n######### Inorder Traversal #########\n");
+    IterateInorder(root);
+    printf("\n######### End ######################\n");
+    printf("\n######### Preorder Traversal #########\n");
+    IteratePreorder(root);
+    printf("\n######### End ######################\n");
+    printf("\n######### Postorder Traversal #########\n");
+    IteratePostorder(root);
+    printf("\n######### End ######################\n");
+    node_t *MinNode = NULL;
+    MinNode = FindMin(root);
+    if(!MinNode)
+        printf("\n Failed to get smallest Node \n");
+    else
         printf("\nSmallest Entry: %d\n", MinNode->data);
-     node_t *MaxNode = NULL;
-     MaxNode = FindMax(root);
-     if(!MaxNode)
-         printf("\nFailed to get Biggest Node \n");
-     else
+    node_t *MaxNode = NULL;
+    MaxNode = FindMax(root);
+    if(!MaxNode)
+        printf("\nFailed to get Biggest Node \n");
+    else
         printf("\nBiggest Entry: %d\n", MaxNode->data);
-     return 0;
+    node_t * findnode = NULL;
+    findnode = Search(root, 95);
+    if(!findnode)
+        printf("\n Node with data 95 not found\n");
+    else
+        printf("\nFound Node with data 95 at %p with data %d\n", findnode, findnode->data);
+    findnode = NULL;
+    findnode = Search(root, 100);
+    if(!findnode)
+        printf("\n Node with data 100 not found\n");
+    else
+        printf("\nFound Node with data 100 at %p with data %d\n", findnode, findnode->data);
+    return 0;
 }
